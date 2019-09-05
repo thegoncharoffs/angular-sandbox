10dd409056afcf78b5e2eb10126f27b6bb87e4ca
bogo1118 <boris.goncharov@netcracker.com>
2019-09-05 18:11:51 +0300

    Svg store added

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require('gulp'); // npm install gulp -g
+const svgstore = require('gulp-svgstore'); // npm install gulp-svgstore --save-dev
+const svgmin = require('gulp-svgmin'); // npm install gulp-svgmin --save-dev
+const inject = require('gulp-inject'); // npm install gulp-inject --save-dev
+const path = require('path'); // installed with npm install gulp -g
+
+gulp.task('svgstore', function () {
+  const svgs = gulp
+    .src('./src/assets/**/*.svg')
+    .pipe(svgmin(function (file) {
+      const prefix = path.basename(file.relative, path.extname(file.relative));
+      return {
+        plugins: [
+          {
+            removeTitle: true
+          },
+          {
+            removeAttrs: {
+              attrs: "(fill|stroke)"
+            }
+          },
+          {
+            removeStyleElement: true
+          },
+          {
+            cleanupIDs: {
+              prefix: prefix + '-',
+              minify: true
+            }
+          }
+        ]
+      }
+    }))
+    .pipe(svgstore({inlineSvg: true}));
+
+  function fileContents(filePath, file) {
+    return file.contents.toString();
+  }
+
+  return gulp
+    .src('./src/index.html')
+    .pipe(inject(svgs, {transform: fileContents}))
+    .pipe(gulp.dest('./src'));
+});
