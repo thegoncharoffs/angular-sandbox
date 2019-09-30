e9b2134d7e321b4ac9c922204edd38cea9f10933
Boris Goncharov <boris.goncharov@netcrakcer.com>
2019-09-30 18:50:47 +0300

    Routing example added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
 import {TabPanelModule} from './components/tab-panel/tab-panel.module';
 import {TabPanelItemModule} from './components/tab-panel/item/tab-panel-item.module';
 import {ExamplesPageModule} from './components/examples-page/examples-page.module';
