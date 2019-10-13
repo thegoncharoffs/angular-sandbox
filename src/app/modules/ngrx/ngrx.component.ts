1ed56c11c88861f63203251cd33d5f042f93a353
Boris Goncharov <boris.goncharov@netcrakcer.com>
2019-10-13 17:37:49 +0300

    NGRX Example added

---
+++ src/app/modules/ngrx/ngrx.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {AppState} from './store/app.state';
+import {select, Store} from '@ngrx/store';
+import {selectCounterLabel, selectCounterValue} from './store/selectors/counter.selectors';
+import {counterDecrease, counterIncrease, counterSet} from "./store/actions/counter.actions";
+
+@Component({
+  selector: 'app-ngrx',
+  templateUrl: './ngrx.component.html',
+  styleUrls: ['./ngrx.component.scss']
+})
+export class NgrxComponent implements OnInit {
+  public value$: Observable<number>;
+  public label$: Observable<string>;
+
+  constructor(private store: Store<AppState>) {
+  }
+
+  ngOnInit() {
+    this.value$ = this.store.pipe(select(selectCounterValue));
+    this.label$ = this.store.pipe(select(selectCounterLabel));
+  }
+
+  public _increase(): void {
+    this.store.dispatch(counterIncrease());
+  }
+
+  public _decrease(): void {
+    this.store.dispatch(counterDecrease());
+  }
+
+  public _setValue(): void {
+    this.store.dispatch(counterSet({value: 20}));
+  }
+}
